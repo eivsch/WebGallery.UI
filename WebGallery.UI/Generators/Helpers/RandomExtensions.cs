fb9bbbbc636f1370cda40f3ef3e7744ab6275a0c
Eivind Schulstad <eivind.schulstad@pragroup.no>
2020-05-08 17:21:39 +0200

    added soe helper classes

---
+++ WebGallery.UI/Generators/Helpers/RandomExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebGallery.UI.Generators.Helpers
+{
+    public static class RandomExtensions
+    {
+        public static void Shuffle<T>(this IList<T> list, Random rng)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}
---
+++ WebGallery.UI/Generators/Helpers/RowFormats.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebGallery.UI.Generators.Helpers
+{
+    public static class RowFormats
+    {
+        public static int[][] Format => new int[][]
+            {
+                // Valid with 4 items
+                new int[] { 3, 3, 3, 3 },
+                // Valid with 3 items
+                new int[] { 4,4,4 },
+                new int[] { 6,3,3 },
+                new int[] { 3,6,3 },
+                new int[] { 3,3,6 },
+                // Valid with 2 items
+                new int[] { 6,6 },
+                new int[] { 4,8 },
+                new int[] { 8,4 },
+            };
+    }
+}
