2f848e057f638e266b90ad7140b7751a1b19b8e3
Eivind Schulstad <eivind.schulstad@pragroup.no>
2020-11-28 15:39:08 +0100

    Finalized async loading of info cards

---
+++ WebGallery.UI/wwwroot/js/async-view-loader.js
@@ -0,0 +1,8 @@
+﻿function loadAsyncViews() {
+    $('[data-async-view]').each(function (index, item) {
+        var url = $(item).data('url');
+        if (url && url.length > 0) {
+            $(item).load(url);
+        }
+    });
+}
