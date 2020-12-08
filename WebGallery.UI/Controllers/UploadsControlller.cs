8c96b786e78e1f903c98bb4dd9c5e75948587146
Eivsch <eivindschulstad@gmail.com>
2020-12-08 22:30:20 +0100

    Working upload logic - simplified

--- WebGallery.UI/Controllers/UploadsControlller.cs
+++ WebGallery.UI/Controllers/UploadsControlller.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Application.Services.Interfaces;
@@ -26,6 +27,19 @@ namespace WebGallery.UI.Controllers
         [HttpPost]
         public async Task<IActionResult> Post(IEnumerable<IFormFile> filesToUpload)
         {
+            var rootPath = "/var/www/pics";
+            var newFolder = "folder1";
+            
+            var newDir = Path.Combine(rootPath, newFolder);
+            Directory.CreateDirectory(newDir);
+
+            foreach (var file in filesToUpload)
+            {
+                var filePath = Path.Combine(newDir, file.FileName);
+
+                file.CopyTo(new FileStream(filePath, FileMode.Create));
+            }
+
             return View();
         }
     }
